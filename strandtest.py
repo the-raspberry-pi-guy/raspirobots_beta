b9bb08972da9e870d3132bd27a9d963b802611e4
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2018-08-30 14:26:54 +0000

    Add LED strip line

--- strandtest.py
+++ strandtest.py
@@ -20,6 +20,7 @@ LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP      = ws.WS2811_STRIP_GRB # Strip type and color ordering
 
 # Define functions which animate LEDs in various ways.
 def colorWipe(strip, color, wait_ms=50):
