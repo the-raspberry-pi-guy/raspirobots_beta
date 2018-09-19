dd0e028b49d64a5952248d8342f103958030a24d
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2018-09-19 17:10:27 +0100

    Remove unnecessary time import and comments

--- line_follower.py
+++ line_follower.py
@@ -1,5 +1,4 @@
 import gpiozero
-import time
 
 SPEED = 0.25
 
@@ -9,15 +8,11 @@ left = gpiozero.DigitalInputDevice(9)
 right = gpiozero.DigitalInputDevice(11)
 
 while True:
-	# Both sensors read white
 	if (left.is_active == True) and (right.is_active == True):
 		robot.forward(SPEED)
-	# Left sensor detects the line
 	elif (left.is_active == False) and (right.is_active == True):
 		robot.right(SPEED)
-	# Right sensor detects the line
 	elif (left.is_active == True) and (right.is_active == False):
 		robot.left(SPEED)
-	# Only option left: both sensors detect line
 	else:
 		robot.stop()
